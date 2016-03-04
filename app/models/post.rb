a91654bc57d2d9a2e199ec4f39e9c824d1e601f8
Joshua Archer <josharcher@me.com>
2016-03-04 12:49:32 -0800

    add polymorphic comments model, and fix accompanying tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :subreddit
-  has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_many :comments, as: :parent,
+                      class_name: "Comment",
+                      dependent: :destroy
   validates :subreddit_id, presence: true
   validates :user_id, presence: true
   validates :title, presence: true
+  validates_format_of :link, with: URI::regexp(%w(http https)),
+                             if: lambda { |post| post.link.present? }
 end
--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -3,17 +3,10 @@ class Subreddit < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   validates :user_id, presence: true
   validates :name, presence: true,
-                   length: { minimum: 3, maximum: 30 },
+                   format: { with: /^[a-zA-Z0-9]*$/, multiline: true},
+                   length: { maximum: 21, minimum: 4},
                    uniqueness: { case_sensitive: false },
-                   format: { with: /^[A-Za-z0-9.&]*\z/,
-                             multiline: true }
-  # validates :name, presence: true,
-  #                  uniqueness: { case_sensitive: false },
-  #                  length: { maximum: 21,
-  #                            too_long: "name must be less than %{count} characters",
-  #                            minimum: 4,
-  #                            too_short: "name must be at least %{count} characters" },
-  #                  format: { with: "/^[a-zA-Z\d\s]*$/", multiline: true}
-                   #^ validates there are no special characters
+                   format: { with: /^[a-zA-Z0-9]*$/, multiline: true}
+                   #  ^ validates there are no special characters
   validates :title, presence: true
 end
