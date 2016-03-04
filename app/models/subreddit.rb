1501c7f444d0d79f8a6a5092fe3b03f08eb1562b
Joshua Archer <josharcher@me.com>
2016-03-03 22:41:24 -0800

    add subreddit validations

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -2,6 +2,13 @@ class Subreddit < ActiveRecord::Base
   belongs_to :user
   has_many :posts, dependent: :destroy
   validates :user_id, presence: true
-  validates :name, presence: true
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false },
+                   length: { maximum: 21,
+                             too_long: "name must be less than %{count} characters",
+                             minimum: 4,
+                             too_short: "name must be at least %{count} characters" },
+                   format: { with: "/^[a-zA-Z\d\s]*$/"}
+                   #^ validates there are no special characters
   validates :title, presence: true
 end
